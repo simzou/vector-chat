17d04af248bca0b27fe0ad36bcfe8978cc8f76fa
Howard Huang <howardhuang96@gmail.com>
2018-03-02 13:26:23 -0800

    sudmodule in setup script

--- VagrantSetup/initial.sh
+++ VagrantSetup/initial.sh
@@ -23,6 +23,9 @@ sudo apt-get install autoconf automake libtool curl make g++ unzip git wget pyth
 # ndn-cxx
 sudo apt-get install build-essential libcrypto++-dev libsqlite3-dev libboost-all-dev libssl-dev -y
 
+# initialize our submodules
+sudo git submodule init
+
 # protobuf
 wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.zip && unzip protobuf-cpp-3.5.1.zip
 cd protobuf-3.5.1 
