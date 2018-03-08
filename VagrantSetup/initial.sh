e2af2a94548d67c379a17b90bf47bd2c1591c428
Howard Huang <howardhuang96@gmail.com>
2018-03-08 12:18:26 -0800

    change setup

--- VagrantSetup/initial.sh
+++ VagrantSetup/initial.sh
@@ -24,7 +24,7 @@ sudo apt-get install autoconf automake libtool curl make g++ unzip git wget pyth
 sudo apt-get install build-essential libsqlite3-dev libboost-all-dev libssl-dev -y
 
 # initialize our submodules and retrieve their contents
-cd /home/vectorchat
+cd /home/vagrant/vectorchat
 
 sudo git submodule init
 sudo git submodule update
@@ -34,7 +34,7 @@ cd protobuf
 sudo sh -c './autogen.sh && ./configure && make && make install && ldconfig'
 
 # ndn-cxx
-cd ../ndn-cxx
+cd /home/vagrant/vectorchat/ndn-cxx
 
 # I was running out of memory to compile various libraries, adding swap space fixed this issue 
 sudo fallocate -l 4G /swapfile
@@ -45,22 +45,8 @@ sudo swapon /swapfile
 sudo sh -c "./waf configure && ./waf && ./waf install"
 
 # vectorsync
-# cd ../VectorSync
+cd /home/vagrant/vectorchat/VectorSync
 
-# ./waf configure 
-# TODO: this is giving the following error:
-# -----------------------------------------------
-# Setting top to                           : /home/vagrant/vectorchat/VectorSync
-# Setting out to                           : /home/vagrant/vectorchat/VectorSync/build
-# Checking for 'g++' (C++ compiler)        : /usr/bin/g++
-# Checking supported CXXFLAGS              : -std=c++11
-# Checking supported CXXFLAGS              : -O2 -g -pedantic -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers
-# Checking for program 'pkg-config'        : /usr/bin/pkg-config
-# Checking for 'protobuf'                  : yes
-# Checking for program 'protoc'            : /usr/local/bin/protoc
-# Checking for 'libndn-cxx'                : not found
-# The configuration failed
-# (complete log in /home/vagrant/vectorchat/VectorSync/build/config.log)
-# -----------------------------------------------
+sudo sh -c "./waf configure && ./waf && ./waf install"
 
 
